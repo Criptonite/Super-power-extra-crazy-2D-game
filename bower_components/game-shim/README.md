76618a99bffad3fb44fc30744bfd7e198b5f05c2
Guba Dmitriy <gudmian@gmai.com>
2017-11-14 18:13:25 +0300

    Configure project dependencies

---
+++ bower_components/game-shim/README.md
@@ -0,0 +1,14 @@
+Game Shim
+=======================
+
+Simple collection of shims to normalize several game-realated APIs into their documented standards.
+
+APIs Covered
+-------------------------
+
+ - requestAnimationFrame
+ - fullscreen
+ - mouselock
+ - gamepad
+ - high-resolution timers
+
--- master-server.js
+++ master-server.js
@@ -23,53 +23,10 @@ app.use('/', router);
 router.get('/', function (req, res, next) {
     res.render("index");
 });
-//
-// function getFile(filename, res) {
-//     fs.exists(filename, function (exists) {
-//         function error(status, message) {
-//             res.writeHead(status, {"Content-Type": "text/plain"});
-//             res.write(message);
-//             res.end();
-//         }
-//
-//         if (!exists) return error(404, "404 Not Found\n");
-//
-//         fs.readFile(filename, "binary", (err, file) => {
-//             if (err) return error(500, err + "\n");
-//
-//             if (path.extname(filename) === ".js") {
-//                 res.writeHead(200, {"Content-Type": "text/javascript"});
-//                 res.write(file, "binary");
-//                 res.end();
-//             }
-//             else if (path.extname(filename) === ".css") {
-//                 res.writeHead(200, {"Content-Type": "text/css"});
-//                 res.write(file, "binary");
-//                 res.end();
-//             }
-//             else return error(403, "Access denied");
-//         });
-//     });
-// }
 
 
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//     let err = new Error("Page not found");
-//     err.status = 404;
-//     next(err);
-// });
+router.post
 
-// error handler
-// app.use(function (err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = err;
-//
-//     // render the error page
-//     res.status(err.status || 500);
-//     res.render("error");
-// });
 
 
 http.createServer(app).listen(8080);
